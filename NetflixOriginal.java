5eeb51ad6cfe8f0a2188aeea4dfb4d1450ca0db6
Adams1234784 <adam.d.szloboda@gmail.com>
2025-07-30 13:59:42 -0700

    Create NetflixOriginal.java

---
+++ NetflixOriginal.java
@@ -0,0 +1,31 @@
+public class NetflixOriginal {
+    private String name;
+    private double starRating;
+    private String genre;
+
+    public NetflixOriginal() {}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getStarRating() {
+        return starRating;
+    }
+
+    public void setStarRating(double starRating) {
+        this.starRating = starRating;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
